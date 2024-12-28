87bc26d33d7879add27518b4af7c2bf96c87c219
FlackCode <fl4ckc0de@gmail.com>
2024-12-28 13:34:04 +0200

    Made mario jump

--- public/js/main.js
+++ public/js/main.js
@@ -6,12 +6,6 @@ import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
 import Timer from "./Timer.js";
 import Keyboard from "./KeyboardState.js";
 
-const input = new Keyboard();
-input.addMapping(32, keyState => {
-    console.log(keyState);
-})
-input.listenTo(window);
-
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
@@ -30,7 +24,18 @@ Promise.all([
 
     const gravity = 2000;
     mario.pos.set(64, 180);
-    mario.vel.set(200, -600);
+
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+        if (keyState) {
+            mario.jump.start();
+        } else {
+            mario.jump.cancel();
+        }
+        console.log(keyState);
+    })
+    input.listenTo(window);
 
     const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
