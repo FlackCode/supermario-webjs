945a32c6b3c1e1b288c57c44a3f09241635be35d
FlackCode <fl4ckc0de@gmail.com>
2024-12-28 12:31:37 +0200

    Added timer accuracy

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,19 @@
 import Compositor from "./Compositor.js";
-import Entity from "./Entity.js";
 import { loadLevel } from "./loaders.js";
-import { loadMarioSprite, loadBackgroundSprites } from "./sprites.js";
-import { createBackgroundLayer } from "./layers.js";
+import { createMario } from "./entities.js";
+import { loadBackgroundSprites } from "./sprites.js";
+import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
+import Timer from "./Timer.js";
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-function createSpriteLayer(entity) {
-    return function drawSpriteLayer(context) {
-        entity.draw(context);
-    }
-}
-
 Promise.all([
-    loadMarioSprite(),
+    createMario(),
     loadBackgroundSprites(),
     loadLevel('1-1')
 ]).then(([
-    marioSprite,
+    mario,
     backgroundSprites,
     level,
 ]) => {
@@ -26,28 +21,18 @@ Promise.all([
     const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
     comp.layers.push(backgroundLayer);
 
-    const gravity = 0.5;
-    const mario = new Entity();
+    const gravity = 30;
     mario.pos.set(64, 180);
-    mario.vel.set(2, -10);
-
-    mario.draw = function drawMario(context) {
-        marioSprite.draw("idle", context, this.pos.x, this.pos.y);
-    }
-    mario.update = function updateMario() {
-        this.pos.x += this.vel.x;
-        this.pos.y += this.vel.y;
-    }
+    mario.vel.set(200, -600);
 
     const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
 
-    function update() {
+    const timer = new Timer(1/60);
+    timer.update = function update(deltaTime) {
         comp.draw(context);
-        mario.update();
+        mario.update(deltaTime);
         mario.vel.y += gravity;
-        requestAnimationFrame(update);
     }
-
-    update();
+    timer.start();
 });
