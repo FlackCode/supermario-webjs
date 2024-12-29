d3102dffd6a6dafb6017cfc87a9dabf17661ab9b
FlackCode <fl4ckc0de@gmail.com>
2024-12-29 10:17:26 +0200

    Cleaned up code to begin working on collisions

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,3 +1,6 @@
+import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
+import { loadBackgroundSprites } from "./sprites.js";
+import Level from "./Level.js";
 export function loadImage(url) {
     return new Promise(resolve => {
         const image = new Image();
@@ -9,5 +12,17 @@ export function loadImage(url) {
 }
 
 export function loadLevel(name) {
-    return fetch(`levels/${name}.json`).then(r => r.json());
+    return Promise.all([
+        fetch(`levels/${name}.json`).then(r => r.json()),
+        loadBackgroundSprites(),
+    ]).then(([levelSpec, backgroundSprites]) => {
+        const level = new Level();
+        const backgroundLayer = createBackgroundLayer(levelSpec.backgrounds, backgroundSprites);
+        level.comp.layers.push(backgroundLayer);
+    
+        const spriteLayer = createSpriteLayer(level.entities);
+        level.comp.layers.push(spriteLayer);
+
+        return level;
+    })
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,5 @@
-import Compositor from "./Compositor.js";
 import { loadLevel } from "./loaders.js";
 import { createMario } from "./entities.js";
-import { loadBackgroundSprites } from "./sprites.js";
-import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
 import Timer from "./Timer.js";
 import Keyboard from "./KeyboardState.js";
 
@@ -11,19 +8,15 @@ const context = canvas.getContext("2d");
 
 Promise.all([
     createMario(),
-    loadBackgroundSprites(),
     loadLevel('1-1')
 ]).then(([
     mario,
-    backgroundSprites,
     level,
 ]) => {
-    const comp = new Compositor();
-    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-    comp.layers.push(backgroundLayer);
-
     const gravity = 2000;
     mario.pos.set(64, 180);
+
+    level.entities.add(mario);
 
     const SPACE = 32;
     const input = new Keyboard();
@@ -37,13 +30,12 @@ Promise.all([
     })
     input.listenTo(window);
 
-    const spriteLayer = createSpriteLayer(mario);
-    comp.layers.push(spriteLayer);
+    
 
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime) {
-        comp.draw(context);
-        mario.update(deltaTime);
+        level.update(deltaTime);
+        level.comp.draw(context);
         mario.vel.y += gravity * deltaTime;
     }
     timer.start();
