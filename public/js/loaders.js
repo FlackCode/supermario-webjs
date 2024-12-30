6dab1135220bc88ce926acbed1fcb4e5478f38f0
FlackCode <fl4ckc0de@gmail.com>
2024-12-30 18:23:14 +0200

    Added pipes

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -20,35 +20,6 @@ function loadJSON(url) {
     return fetch(url).then(r => r.json());
 }
 
-export function createTiles(level, backgrounds) {
-    function applyRange(background, xStart, xLen, yStart, yLen) {
-        const xEnd = xStart + xLen;
-        const yEnd = yStart + yLen;
-        for (let x = xStart; x < xEnd; x++) {
-            for (let y = yStart; y < yEnd; y++) {
-                level.tiles.set(x, y, {
-                    name: background.tile,
-                    type: background.type,
-                });
-            }
-        }
-    }
-    backgrounds.forEach(background => {
-        background.ranges.forEach(range => {
-            if (range.length === 4) {
-                const [xStart, xLen, yStart, yLen] = range;
-                applyRange(background, xStart, xLen, yStart, yLen);
-            } else if (range.length === 3) {
-                const [xStart, xLen, yStart] = range;
-                applyRange(background, xStart, xLen, yStart, 1);
-            } else if (range.length === 2) {
-                const [xStart, yStart] = range;
-                applyRange(background, xStart, 1, yStart, 1);
-            }
-        });
-    })
-}
-
 export function loadSpriteSheet(name) {
     return loadJSON(`sprites/${name}.json`)
     .then(sheetSpec => Promise.all([
@@ -88,7 +59,7 @@ export function loadLevel(name) {
     .then(([levelSpec, backgroundSprites]) => {
         const level = new Level();
 
-        createTiles(level, levelSpec.backgrounds);
+        createTiles(level, levelSpec.backgrounds, levelSpec.patterns);
         const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
         level.comp.layers.push(backgroundLayer);
     
@@ -96,5 +67,41 @@ export function loadLevel(name) {
         level.comp.layers.push(spriteLayer);
 
         return level;
+    })
+}
+
+export function createTiles(level, backgrounds, patterns, offsetX = 0, offsetY = 0) {
+    function applyRange(background, xStart, xLen, yStart, yLen) {
+        const xEnd = xStart + xLen;
+        const yEnd = yStart + yLen;
+        for (let x = xStart; x < xEnd; x++) {
+            for (let y = yStart; y < yEnd; y++) {
+                const derivedX = x + offsetX;
+                const derivedY = y + offsetY;
+                if (background.pattern) {
+                    const backgrounds = patterns[background.pattern].backgrounds;
+                    createTiles(level, backgrounds, patterns, derivedX, derivedY);
+                } else {
+                    level.tiles.set(derivedX, derivedY, {
+                        name: background.tile,
+                        type: background.type,
+                    });
+                }
+            }
+        }
+    }
+    backgrounds.forEach(background => {
+        background.ranges.forEach(range => {
+            if (range.length === 4) {
+                const [xStart, xLen, yStart, yLen] = range;
+                applyRange(background, xStart, xLen, yStart, yLen);
+            } else if (range.length === 3) {
+                const [xStart, xLen, yStart] = range;
+                applyRange(background, xStart, xLen, yStart, 1);
+            } else if (range.length === 2) {
+                const [xStart, yStart] = range;
+                applyRange(background, xStart, 1, yStart, 1);
+            }
+        });
     })
 }
