0f4c16d50d874075ac1bc3b1d685a107508c0ddb
FlackCode <fl4ckc0de@gmail.com>
2025-01-03 20:34:46 +0200

    Fixed dashboard

--- public/js/layers/dashboard.js
+++ public/js/layers/dashboard.js
@@ -1,12 +1,30 @@
-export function createDashboardLayer(font, playerEnv) {
+import { findPlayers } from "../player.js";
+
+function getPlayerTrait(level) {
+    for (const entity of findPlayers(level)) {
+        return entity.player;
+    }
+}
+
+function getTimerTrait(level) {
+    for (const entity of level.entities) {
+        if (entity.levelTimer) {
+            return entity.levelTimer;
+        }
+    }
+}
+
+export function createDashboardLayer(font, level) {
     const LINE1 = font.size;
     const LINE2 = font.size * 2;
-    const coins = 13;
 
     return function drawDashboard(context) {
-        const {score, time} = playerEnv.playerController;
+        const playerTrait = getPlayerTrait(level);
+        const timerTrait = getTimerTrait(level);
+        const {currentTime} = timerTrait;
+        const {score, coins} = playerTrait;
 
-        font.print("MARIO", context, 16, LINE1);
+        font.print(playerTrait.name, context, 16, LINE1);
         font.print(score.toString().padStart(6, "0"), context, 16, LINE2);
 
         font.print("@x" + coins.toString().padStart(2, "0"), context, 96, LINE2);
@@ -15,6 +33,6 @@ export function createDashboardLayer(font, playerEnv) {
         font.print("1-1", context, 160, LINE2);
 
         font.print("TIME", context, 208, LINE1);
-        font.print(time.toFixed().toString().padStart(3, "0"), context, 216, LINE2);
+        font.print(currentTime.toFixed().toString().padStart(3, "0"), context, 216, LINE2);
     }
 }
